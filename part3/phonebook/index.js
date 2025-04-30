c41904cebe3a3ac15f54531e235e1fa249bf7092
SIVAKUMAR1267 <sivakumar.r.6324@gmail.com>
2025-04-30 20:56:30 +0530

    ex  3.5,3.6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 let persons = [
     
         { 
@@ -25,6 +26,37 @@ let persons = [
     
 ]
 
+const generateId = () => {
+  const key = Math.random().toString(36).substring(2, 15);
+  return key
+}
+
+
+  app.post('/api/persons', (request, response) => {
+    const body = request.body
+    
+      if (!body.name || !body.number) {
+        return response.status(400).json({ 
+          error: 'name or number missing' 
+        })
+      }
+      if (persons.find(person => person.name === body.name)) {
+        return response.status(400).json({ 
+          error: 'name must be unique' 
+        })
+      }
+    
+      const person = {
+        "name": body.name,
+        "number": body.number,
+        id: generateId(),
+      }
+    
+    persons = persons.concat(person)
+    
+    response.json(person)
+    })
+
 app.get('/info', (request, response) => {
     const date = new Date()
     const totalPersons = persons.length
@@ -32,7 +64,7 @@ app.get('/info', (request, response) => {
     response.send(info)
   })
 
-  app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response) => {
     const id = request.params.id
     persons = persons.filter( person => person.id !== id)
   
@@ -57,6 +89,6 @@ app.get('/api/persons', (request, response) => {
   })
   
   const PORT = 3001
-  app.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
